473a5f9effd197672932b0a58a2e62fd944fa42e
elkazajo <mzhumakov@gmail.com>
2025-06-03 15:05:09 +0500

    added embedder

---
+++ embedder.py
@@ -0,0 +1,17 @@
+import torch
+from sentence_transformers import SentenceTransformer
+import os
+
+_MODEL = None
+
+def get_model():
+    global _MODEL
+    if _MODEL is None:
+        _MODEL = SentenceTransformer(os.getenv("EMBEDDING_MODEL_PATH", "local_models/intfloat/multilingual-e5-small"))
+    return _MODEL
+
+def generate_embeddings(chunks):
+    model = get_model()
+    texts = ["passage: " + (chunk.get("description") or chunk.get("name") or "") for chunk in chunks]
+    embs = model.encode(texts, convert_to_numpy=True, normalize_embeddings=True)
+    return embs
